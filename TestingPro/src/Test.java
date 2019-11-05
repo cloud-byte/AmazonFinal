7788fe9b733fbd05d35e1b5488c9e7cd3a996356
ingenyan <luialonso@externosdeloittemx.com>
2019-11-05 12:26:38 -0600

    ferweyx2

--- TestingPro/src/Test.java
+++ TestingPro/src/Test.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;;
 public class Test {
 
 	/**
-	 * @param argsguiuyiyuyu
+	 * @param argsguiuyiyuyuasdas
 	 */
 	public static void main (String [] args) {
 		// TODO Auto-generated constructor stub
