70702b948cfb89ee1c488c0fd9b0c57ac042abaf
ingenyan <luialonso@externosdeloittemx.com>
2019-11-06 12:48:04 -0600

    Update Amazon Search Map

---
+++ src/com/deloitte/test/amazon/page/home/AmazonSearchResult.java
@@ -0,0 +1,55 @@
+package com.deloitte.test.amazon.page.home;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AmazonSearchResult {
+	public void clickElement(WebDriver driver, int indexElement) {
+		driver.findElement(By.xpath("//span[@class='celwidget slot=SEARCH_RESULTS template=SEARCH_RESULTS widgetId=search-results index="+indexElement+"']//img[@class='s-image']")).click();
+	}
+	public void clickBrandFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_89')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}
+	public void clickPriceFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_36')]["+indexElement+"]//a[@class='a-link-normal s-navigation-item']")).click();
+	}
+	public void clickDisplaySizeFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_size_browse-bin')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}
+	public void clickTechnologyFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_feature_nineteen_browse-bin')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}
+	public void clickConnectivityTypeSizeFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_feature_four_browse-bin')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}
+	public void clickSOFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_operating_system_browse-bin')]["+indexElement+"]//a[@class='a-link-normal s-navigation-item']")).click();
+	}
+	public void clickFlashMemoryFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_feature_nine_browse-bin')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}
+	public void clickFeatureKeywordsFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_feature_keywords_browse-bin')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}
+	public void clickRamFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_feature_five_browse-bin')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}
+	public void clickWeightFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_size_two_browse-vebin')]["+indexElement+"]//a[@class='a-link-normal s-navigation-item']")).click();
+	}
+	public void clickConditionFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_condition-type')]["+indexElement+"]//a[@class='a-link-normal s-navigation-item']")).click();
+	}
+	public void clickNewUpcomingFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_date')]["+indexElement+"]//a[@class='a-link-normal s-navigation-item']")).click();
+	}
+	public void clickGLobalStoreFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_global_store_origin_marketplace')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}
+	public void clickSellerFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_6')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}
+	public void clickAvailabilityFilter(WebDriver driver,int indexElement) {
+		driver.findElement(By.xpath("//li[starts-with(@id,'p_n_availability')]["+indexElement+"]//i[@class='a-icon a-icon-checkbox']")).click();
+	}	
+}
--- src/com/deloitte/test/amazon/tests/Test1Amazon.java
+++ src/com/deloitte/test/amazon/tests/Test1Amazon.java
@@ -5,15 +5,19 @@ import org.openqa.selenium.WebDriver;
 import com.deloitte.test.amazon.configuracion.Configuracion;
 import com.deloitte.test.amazon.page.home.AmazonFooter2;
 import com.deloitte.test.amazon.page.home.AmazonHome;
+import com.deloitte.test.amazon.page.home.AmazonSearchResult;
 public class Test1Amazon {
 	static AmazonHome ama = new AmazonHome();
 	static AmazonFooter2 amaft2 = new AmazonFooter2();
+	static AmazonSearchResult amaR = new AmazonSearchResult();
 	
 	public static void main (String [] args) throws InterruptedException {
 		Configuracion conf = new Configuracion();
 		WebDriver driver = conf.getDriver();
 		JavascriptExecutor js = conf.getJs();
-		
+		ama.findBar(driver);
+		amaR.clickDisplaySizeFilter(driver, 1);
+
 		
 	
 		
