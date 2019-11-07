9e06b28afdb3216bacfaa79f35b83e2cda5338dd
Gerardo Castañon <gerardocasta9@gmail.com>
2019-11-07 11:21:44 -0600

    Update Gerr´s PUBS

---
+++ TestingPro/src/com/deloitte/test/amazon/page/home/AmazonSignIn.java
@@ -0,0 +1,45 @@
+package com.deloitte.test.amazon.page.home;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+public class AmazonSignIn{
+	private WebDriver driver; 
+	
+	public AmazonSignIn(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void InputEmail(String email) {
+		driver.findElement(By.xpath("//input[@id='ap_email']")).sendKeys(email);
+	}
+	public void ClickContinue() {
+		driver.findElement(By.xpath("//input[@id='continue']")).click();
+	}
+	public void getFail() {
+		String a = driver.findElement(By.xpath("//div[@id='auth-error-message-box']//div[@class='a-box-inner a-alert-container']")).getText();
+		System.out.println(a);
+	}
+	public void clickCreateAccount() {
+		driver.findElement(By.xpath("//a[@id='createAccountSubmit']	")).click();
+	}
+	public void RegisterForm(int noElement,String Data)
+	{
+		List<WebElement> myElements = driver.findElements(By.xpath("//input[@id]"));
+		System.out.println(myElements.get(noElement).getText());
+		
+		if(noElement== 4) {
+        myElements.get(noElement).click();
+        }
+		else {
+	    myElements.get(noElement).sendKeys(Data);	
+		}
+	
+	//input[@id='ap_customer_name']
+	
+	
+	
+}
+}
--- TestingPro/src/com/deloitte/test/amazon/tests/Test1Amazon.java
+++ TestingPro/src/com/deloitte/test/amazon/tests/Test1Amazon.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import com.deloitte.test.amazon.configuracion.Configuracion;
 import com.deloitte.test.amazon.page.home.AmazonFooter;
 import com.deloitte.test.amazon.page.home.AmazonHome;
+import com.deloitte.test.amazon.page.home.AmazonSignIn;
 
 import java.util.logging.Logger;
 public class Test1Amazon {
@@ -13,8 +14,19 @@ public class Test1Amazon {
 		JavascriptExecutor js = cnf.getJs();
 		AmazonFooter amaFt = new AmazonFooter(cnf.getDriver());
 		AmazonHome amaHome = new AmazonHome(amaFt.getDriver());
-		amaFt.clickWebElement(2);
+		AmazonSignIn amaSign = new AmazonSignIn(amaFt.getDriver());
 		amaHome.clickSignIn();
+		amaSign.InputEmail("Fernanflo1222@gmail.com");
+		amaSign.ClickContinue();
+		amaSign.getFail();
+		amaSign.clickCreateAccount();
+		amaSign.RegisterForm(0, "Richy");
+		amaSign.RegisterForm(1, "Richy1222@gmail.com");
+		amaSign.RegisterForm(2, "Richy2323");
+		amaSign.RegisterForm(3, "Richy2323");
+		amaSign.RegisterForm(4, "Yoh");
+		
+		
 		
 	}
 
